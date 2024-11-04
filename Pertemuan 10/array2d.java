8fe1b170a37aab8b49e2db1ec4b7af2325c026e8
rizqiaditya222 <160991358+rizqiaditya222@users.noreply.github.com>
2024-11-04 17:35:28 +0700

    Add files via upload

---
+++ Pertemuan 10/array2d.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class array2d {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        String [][] huruf = {{"a","a1","a2"}, {"b","b1","b2"}, {"c","c1"}};
+        int [][] nomor = new int[2][2];
+        System.out.println(huruf[2][1]);
+        nomor [0][0] = 4;
+        nomor [0][1] = 5;
+        
+        nomor [1][0] = 4;
+        nomor [1][1] = 5;
+
+        // for (int i = 0; i<=1;i++){
+        //     for (int j = 0; j<=1;j++){
+        //         System.out.print(nomor[i][j]+" ");
+        //     }
+        //     System.out.println("\n");
+        // }
+
+        // String [][] nomor2 = new String[2][2];
+
+        // for (int i = 0; i<=1;i++){
+        //     for (int j = 0; j<=1;j++){
+        //         nomor2[i][j] = input.nextLine();
+        //     }
+        // }
+
+        // for (int i = 0; i<=1;i++){
+        //     for (int j = 0; j<=1;j++){
+        //         System.out.print(nomor2[i][j]+" ");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // nama = nama2;
+        // nama [0] = "damai";
+
+        // System.out.println(nama2[0]);
+
+        // System.out.println(nama);
+        // System.out.println(huruf[0]);
+
+
+    }
+}
