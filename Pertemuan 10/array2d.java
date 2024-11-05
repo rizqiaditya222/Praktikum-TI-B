ee35b42b9fbca0b555105647300b89c1b0514222
rizqiaditya222 <160991358+rizqiaditya222@users.noreply.github.com>
2024-11-05 20:02:30 +0700

    Update array2d.java

--- Pertemuan 10/array2d.java
+++ Pertemuan 10/array2d.java
@@ -12,34 +12,34 @@ public class array2d {
         nomor [1][0] = 4;
         nomor [1][1] = 5;
 
-        // for (int i = 0; i<=1;i++){
-        //     for (int j = 0; j<=1;j++){
-        //         System.out.print(nomor[i][j]+" ");
-        //     }
-        //     System.out.println("\n");
-        // }
+        for (int i = 0; i<=1;i++){
+            for (int j = 0; j<=1;j++){
+                System.out.print(nomor[i][j]+" ");
+            }
+            System.out.println("\n");
+        }
 
-        // String [][] nomor2 = new String[2][2];
+        String [][] nomor2 = new String[2][2];
 
-        // for (int i = 0; i<=1;i++){
-        //     for (int j = 0; j<=1;j++){
-        //         nomor2[i][j] = input.nextLine();
-        //     }
-        // }
+        for (int i = 0; i<=1;i++){
+            for (int j = 0; j<=1;j++){
+                nomor2[i][j] = input.nextLine();
+            }
+        }
 
-        // for (int i = 0; i<=1;i++){
-        //     for (int j = 0; j<=1;j++){
-        //         System.out.print(nomor2[i][j]+" ");
-        //     }
-        //     System.out.print("\n");
-        // }
-        // nama = nama2;
-        // nama [0] = "damai";
+        for (int i = 0; i<=1;i++){
+            for (int j = 0; j<=1;j++){
+                System.out.print(nomor2[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+        nama = nama2;
+        nama [0] = "damai";
 
-        // System.out.println(nama2[0]);
+        System.out.println(nama2[0]);
 
-        // System.out.println(nama);
-        // System.out.println(huruf[0]);
+        System.out.println(nama);
+        System.out.println(huruf[0]);
 
 
     }
